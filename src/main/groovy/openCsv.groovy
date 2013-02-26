b33461f8c81c2b7a75b9567077c53cdaf354ff02
Justin Ryan <jryan@netflix.com>
2013-02-26 15:31:50 -0700

    Improve row to being an object/map

--- src/main/groovy/openCsv.groovy
+++ src/main/groovy/openCsv.groovy
@@ -1,21 +1,22 @@
 import au.com.bytecode.opencsv.CSVReader
-import au.com.bytecode.opencsv.CSVParser
+import au.com.bytecode.opencsv.CSVWriter
 
 def TEST_FILE_NAME = './../resources/test.csv'
 def TEST_OUTPUT_FILE_NAME = 'testOut.csv'
 
-List<String[]> rows = new CSVReader(new FileReader(new File(TEST_FILE_NAME)), CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_ESCAPE_CHARACTER, CSVParser.DEFAULT_QUOTE_CHARACTER, 1).readAll()
+def reader = new CSVReader(new FileReader(new File(TEST_FILE_NAME)))
+def header = reader.readNext()
 
-List ourImportantColums = rows.drop(1).collect {
-    [it[33], it[37]]
+def rows = reader.readAll().collect { row ->
+    (0..(row.size()-1)).collectEntries { [header[it], row[it]] }
 }
 
-Map<String, String[]> rowsGroupedByDepHour = ourImportantColums.groupBy { it[1] }
+Map rowsGroupedByDepHour = rows.groupBy { it.DEP_TIME_BLK }
 println "Grouped: " + rowsGroupedByDepHour
 
 rowsGroupedByDepHour.each { departureHour, departures ->
     println "DepartureHour (" + departureHour + ") : "
-    def departureDelays = departures.collect {it[0]}
+    def departureDelays = departures.collect {it.DEP_DELAY}
     println departureDelays
     def avgDelayMin = departureDelays.findAll{ delay -> delay =~ /\-?[0-9]+(.\?[0-9]+)?/ }.collect{it.toFloat()}.sum() / departureDelays.size()
     println "Average for this hour: " + avgDelayMin
