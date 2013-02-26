faa271dc149225adac2539fc4224e172ae41424d
Julie Pitt <ojosverdes75@hotmail.com>
2013-02-26 14:44:58 -0700

    Calculating average delay time for a small dataset.

--- src/main/groovy/openCsv.groovy
+++ src/main/groovy/openCsv.groovy
@@ -6,15 +6,25 @@ def TEST_FILE_NAME = './../resources/test.csv'
 def TEST_OUTPUT_FILE_NAME = 'testOut.csv'
 
 List<String[]> rows = new CSVReader(new FileReader(new File(TEST_FILE_NAME)), CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_ESCAPE_CHARACTER, CSVParser.DEFAULT_QUOTE_CHARACTER, 1).readAll()
-def rowsOver100 = rows.findAll {it[1].toInteger() > 100}
 
-File output = new File(TEST_OUTPUT_FILE_NAME)
-if (output.exists())
-{
-    output.delete()
-}
-output.createNewFile()
-output.withWriter { writer ->
-    new CSVWriter(writer).writeAll(rowsOver100)
+List<Float> delayMinutes = rows.drop(1).collect{
+    try {
+        it[33].toFloat()
+    } catch (NumberFormatException e) {
+        0
+    }}
 
-}
+def avgDelayMin = delayMinutes.sum() / delayMinutes.size()
+
+println(avgDelayMin)
+
+//File output = new File(TEST_OUTPUT_FILE_NAME)
+//if (output.exists())
+//{
+//    output.delete()
+//}
+//output.createNewFile()
+//output.withWriter { writer ->
+//    new CSVWriter(writer).writeAll(rowsOver100)
+//
+//}
