a0cc70f657b9e466b742b4a6267bb2ef0b4cecba
Julie Pitt <ojosverdes75@hotmail.com>
2013-02-26 15:42:23 -0700

    Adding a sort by departure hour plus cleaning up a bit.

--- src/main/groovy/openCsv.groovy
+++ src/main/groovy/openCsv.groovy
@@ -1,23 +1,34 @@
 import au.com.bytecode.opencsv.CSVReader
 import au.com.bytecode.opencsv.CSVParser
 
+// Agregation
+    // grouping over columns
+    // entire dataset
+// average/min/max
+// histograms
+
+// Traveller: optimize X based on Y
+// airline/flight for shortest travel time based on departure date
+// airline/flight for least delay based
+// airline least prone to delay
+
 def TEST_FILE_NAME = './../resources/test.csv'
 def TEST_OUTPUT_FILE_NAME = 'testOut.csv'
 
 List<String[]> rows = new CSVReader(new FileReader(new File(TEST_FILE_NAME)), CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_ESCAPE_CHARACTER, CSVParser.DEFAULT_QUOTE_CHARACTER, 1).readAll()
 
-List ourImportantColums = rows.drop(1).collect {
+List ourImportantColums = rows.drop(1).findAll{ it[33] =~ /\-?[0-9]+(.\?[0-9]+)?/ }.collect {
     [it[33], it[37]]
 }
 
 Map<String, String[]> rowsGroupedByDepHour = ourImportantColums.groupBy { it[1] }
 println "Grouped: " + rowsGroupedByDepHour
 
-rowsGroupedByDepHour.each { departureHour, departures ->
+rowsGroupedByDepHour.sort().each { departureHour, departures ->
     println "DepartureHour (" + departureHour + ") : "
     def departureDelays = departures.collect {it[0]}
     println departureDelays
-    def avgDelayMin = departureDelays.findAll{ delay -> delay =~ /\-?[0-9]+(.\?[0-9]+)?/ }.collect{it.toFloat()}.sum() / departureDelays.size()
+    def avgDelayMin = departureDelays.collect{it.toFloat()}.sum() / departureDelays.size()
     println "Average for this hour: " + avgDelayMin
 }
 
