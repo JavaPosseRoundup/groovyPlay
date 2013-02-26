5a7a94315a73f48bf7540842574b471378bf43f9
Andrew Harmel-Law <andrew@harmel-law.com>
2013-02-26 22:18:44 +0000

    Finds out the average delay by departure hour

--- src/main/groovy/openCsv.groovy
+++ src/main/groovy/openCsv.groovy
@@ -1,22 +1,29 @@
 import au.com.bytecode.opencsv.CSVReader
 import au.com.bytecode.opencsv.CSVParser
-import au.com.bytecode.opencsv.CSVWriter
 
 def TEST_FILE_NAME = './../resources/test.csv'
 def TEST_OUTPUT_FILE_NAME = 'testOut.csv'
 
 List<String[]> rows = new CSVReader(new FileReader(new File(TEST_FILE_NAME)), CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_ESCAPE_CHARACTER, CSVParser.DEFAULT_QUOTE_CHARACTER, 1).readAll()
 
-List<Float> delayMinutes = rows.drop(1).collect{
-    try {
-        it[33].toFloat()
-    } catch (NumberFormatException e) {
-        0
-    }}
+List ourImportantColums = rows.drop(1).collect {
+        [it[33], it[37]]
+}
 
-def avgDelayMin = delayMinutes.sum() / delayMinutes.size()
+Map<String, String[]> rowsGroupedByDepHour = ourImportantColums.groupBy { it[1] }
+println "Grouped: " + rowsGroupedByDepHour
 
-println(avgDelayMin)
+rowsGroupedByDepHour.each { departureHour, departures ->
+    println "DepartureHour (" + departureHour + ") : "
+    def departureDelays = departures.collect {it[0]}
+    println departureDelays
+    def avgDelayMin = departureDelays.findAll{ delay -> delay =~ /\-?[0-9]+(.\?[0-9]+)?/ }.collect{it.toFloat()}.sum() / departureDelays.size()
+    println "Average for this hour: " + avgDelayMin
+}
+
+
+//def avgDelayMin = ourImportantColums.sum() / ourImportantColums.size()
+//println(avgDelayMin)
 
 //File output = new File(TEST_OUTPUT_FILE_NAME)
 //if (output.exists())
